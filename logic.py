7b3d9ceeb7797f5ede12b5a4a182a65b5d0623c0
samaralves1 <samaramaria0220@gmail.com>
2026-04-12 21:24:18 -0300

    feat: criando lab

---
+++ logic.py
@@ -0,0 +1,29 @@
+import heapq
+
+def dijkstra(grafo, inicio, fim):
+  
+    distancias = {v: float('inf') for v in grafo}
+    distancias[inicio] = 0
+    predecessores = {v: None for v in grafo}
+    fila = [(0, inicio)]
+    
+    while fila:
+        d_atual, u = heapq.heappop(fila)
+        
+        if u == fim: break
+        if d_atual > distancias[u]: continue
+        
+        for vizinho, peso in grafo[u]:
+            custo = d_atual + peso
+            if custo < distancias[vizinho]:
+                distancias[vizinho] = custo
+                predecessores[vizinho] = u
+                heapq.heappush(fila, (custo, vizinho))
+    
+    rota = []
+    atual = fim
+    while atual is not None:
+        rota.insert(0, atual)
+        atual = predecessores.get(atual)
+    
+    return rota if distancias[fim] != float('inf') else [], distancias[fim]
